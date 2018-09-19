472f4ebc7c220c4e8b8106e3d83e8090e69cf29b
Alan Munson <amunson@costar.com>
2018-09-19 14:30:24 -0400

    Add API data, update server routes to serve data

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
-const os = require("os");
+const path = require("path");
 
 const app = express();
+const dataDir = path.join(__dirname, "../..", "data");
+const countriesDir = path.join(dataDir, "countries");
 
 app.use(express.static("dist"));
-app.get("/api/getUsername", (req, res) =>
-  res.send({ username: os.userInfo().username })
+app.get("/api/countries", (req, res) =>
+  res.sendFile(path.join(dataDir, "countries.json"))
 );
-app.listen(8080, () => console.log("Listening on port 8080!"));
+app.get("/api/country/:id", (req, res) => {
+  const countryId = (req.params.id || "").toLowerCase();
+  if (!countryId) res.status(404).send("Invalid country id");
+  res.sendFile(path.join(countriesDir, `${countryId}.json`));
+});
+app.get("/api/world", (req, res) =>
+  res.sendFile(path.join(dataDir, "world.json"))
+);
+
+app.listen(3001, () => console.log("Listening on port 3001!"));
